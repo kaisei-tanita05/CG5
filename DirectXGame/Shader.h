cd6a0b2110a85253764d6a265dbcfb8a23ec9698
kaisei-tanita05 <k024g0081@g.neec.ac.jp>
2026-06-03 10:51:32 +0900

    00_04を完了

--- DirectXGame/Shader.h
+++ DirectXGame/Shader.h
@@ -1,15 +1,19 @@
 ﻿#pragma once
 #include <d3d12.h>
 #include <string>
+#include <d3dcompiler.h>
+#include <dxcapi.h>
 
 class Shader {
 
 public:
 	// シェーダーファイルを読み込み、コンパイル済みのデータを生成する
-	void Load(const std::wstring& filepath, const std::string& shaderModel);
+	void Load(const std::wstring& filepath, const std::wstring& shaderModel);
+	void LoadDxc(const std::wstring& filepath, const std::wstring& shaderModel);
 
 	// 生成したコンパイル済みのデータを取得する
 	ID3DBlob* GetBlob();
+	IDxcBlob* GetDxcBlob();
 
 	// コンストラクタ
 	Shader();
@@ -18,4 +22,5 @@ public:
 
 private:
 	ID3DBlob* blob_ = nullptr; // コンストラクタで初期化しなくていい　※C++11以降
+	IDxcBlob* dxcBlob_ = nullptr; // コンパイル済みのシェーダーデータ　※外部コンパイル版
 };
